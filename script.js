2e10c605ce10391540ed41a8dd17b413d3a939d7
ProjectIan <gathagu64@gmail.com>
2025-11-18 21:15:08 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Splash screen transition
+setTimeout(() => {
+  document.getElementById('splash').classList.add('hidden');
+  document.getElementById('auth').classList.remove('hidden');
+}, 2500);
