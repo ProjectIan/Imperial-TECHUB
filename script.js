fbd5e3d850def75b6d8bc1d677583c79f4aa967f
ProjectIan <gathagu64@gmail.com>
2025-11-22 20:34:48 +0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// Splash screen transition
-setTimeout(() => {
-  document.getElementById('splash').classList.add('hidden');
-  document.getElementById('auth').classList.remove('hidden');
-}, 2500);
